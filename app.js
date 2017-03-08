3d468d966af77303855cab6926c0f03b3e9240bc
vishnu-dev <thevishnudev@gmail.com>
2017-03-09 00:30:26 +0530

    Version 1

---
+++ app.js
@@ -0,0 +1,58 @@
+
+var http = require('http'),
+    fs = require('fs'),
+    util = require('util'),
+    express = require('express');
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+var app = express();
+var dir =  process.cwd();
+app.use(express.static(dir)); //current working directory
+app.use(express.static(__dirname)); //module directory
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log("Connect to same "+ifname+" and use "+iface.address+":3000 to access videos");
+    }
+    ++alias;
+  });
+});
+
+http.createServer(function (req, res) {
+  var path = 'video.mp4';
+  var stat = fs.statSync(path);
+  var total = stat.size;
+  if (req.headers['range']) {
+    var range = req.headers.range;
+    var parts = range.replace(/bytes=/, "").split("-");
+    var partialstart = parts[0];
+    var partialend = parts[1];
+
+    var start = parseInt(partialstart, 10);
+    var end = partialend ? parseInt(partialend, 10) : total-1;
+    var chunksize = (end-start)+1;
+    console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunksize);
+
+    var file = fs.createReadStream(path, {start: start, end: end});
+    res.writeHead(206, { 'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
+    file.pipe(res);
+  } else {
+    console.log('ALL: ' + total);
+    res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
+    fs.createReadStream(path).pipe(res);
+  }
+}).listen(3000);
+console.log('Server running');
